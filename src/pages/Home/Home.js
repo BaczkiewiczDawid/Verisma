316aab924b46f72fc9e27cca2520616b8113c1a3
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2023-05-24 12:41:41 +0200

    home page display fixes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,31 @@
 import React from "react";
 import BackgroundImage from "../../assets/images/background.png";
-import { Background, Image, Header } from "./Home.styled";
-import Button from '../../components/Button';
+import { Image, Header, Section, Container, Box, SectionBox } from "./Home.styled";
+import Button from "../../components/Button";
 
 const Home = () => {
   return (
-    <>
-      <Background>
-        <Image src={BackgroundImage} />
-      </Background>
+    <Box>
       <Header>
-        <h1>Quick and hassle-<br></br>free shopping</h1>
-        <Button text="Shop now" />
+        <Image src={BackgroundImage} />
+        <Container>
+          <h1>
+            Quick and hassle-<br></br>free shopping
+          </h1>
+          <Button text="Shop now" />
+        </Container>
       </Header>
-    </>
+      <Section>
+        <SectionBox>
+          <h2>An extension of your closet</h2>
+          <p>
+            Mintmade Fashion offers same-day or next-day delivery within our
+            service areas. Mintmade Fashion offers same-day or next-day delivery
+            within our service areas.
+          </p>
+        </SectionBox>
+      </Section>
+    </Box>
   );
 };
 
